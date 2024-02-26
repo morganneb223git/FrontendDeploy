b6352ea290a110e6748ed910732a5efba98df4be
Morganne Brown <morganneb223mit@gmail.com>
2024-02-26 15:05:27 -0600

    maybe fixed deployment

--- src/index.js
+++ src/index.js
@@ -50,7 +50,7 @@ root.render(
       domain={process.env.REACT_APP_AUTH0_DOMAIN}
       clientId={process.env.REACT_APP_AUTH0_CLIENT_ID}
       authorizationParams={{
-        redirect_uri: window.location.origin
+        redirect_uri: REACT_APP_AUTH0_REDIRECT_URI
       }}>
       <HashRouter>
         <App />
